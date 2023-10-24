5bad8c1cfbf51bfd13c2d5d9185cbd25940bb90d
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2023-10-24 09:06:29 -0300

    feat: Create dinamic card component

---
+++ components/Blog/Card.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+defineProps<{
+  image: string;
+  title: string;
+  excerpt?: string;
+  slug: string;
+}>();
+</script>
+
+
+<template>
+  <div class="col-span-1 shadow-lg">
+    <!-- Blog image -->
+    <div class="grid__image h-[180px] w-full relative overflow-hidden">
+      <img
+        v-if="image"
+        class="w-full h-full absolute object-cover"
+        :src="image"
+        alt="Blog thumb"
+      />
+    </div>
+    <!-- Blog contents -->
+    <div class="p-5">
+      <h3 class="text-xl font-semibold mb-3">{{ title }}</h3>
+      <p v-if="excerpt" class="text-sm mb-3">{{ excerpt }}</p>
+      <nuxt-link
+        class="border-b-2 border-primary-500 inline-flex items-center"
+        :to="`/${slug}`"
+        >Leia mais
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-4 h-4 ml-1 group-hover:ml-2 duration-200"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+          ></path>
+        </svg>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
